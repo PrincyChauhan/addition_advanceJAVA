edfcdb7fa144f76495907c8376b293dce5e49688
PrincyChauhan <66620521+PrincyChauhan@users.noreply.github.com>
2021-01-03 18:07:18 +0530

    DONE

---
+++ src/Add.java
@@ -0,0 +1,77 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class Add
+ */
+@WebServlet("/Add")
+public class Add implements Servlet {
+
+    /**
+     * Default constructor. 
+     */
+    public Add() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see Servlet#getServletInfo()
+	 */
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null; 
+	}
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String a= request.getParameter("num1");
+		String b= request.getParameter("num2");
+		
+	    int x,y,z;
+	    
+	    x=Integer.parseInt(a);
+	    y=Integer.parseInt(b);
+	
+	
+		z= x+y;
+		
+		out.print("sum is" + z);
+	}
+
+}
